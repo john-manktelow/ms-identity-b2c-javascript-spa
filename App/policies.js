f1d902ebd9e670a7dcfece7066cabd26fb17739a
John Manktelow <john+github@manktelow.co.nz>
2023-05-31 09:37:30 +1200

    set signUpSignIn user flow to the name we use (‘B2C_1_signup’)
    (remove unused editProfile flow too)

--- App/policies.js
+++ App/policies.js
@@ -5,16 +5,12 @@
  */
 const b2cPolicies = {
     names: {
-        signUpSignIn: "B2C_1_susi_reset_v2",
-        editProfile: "B2C_1_edit_profile_v2"
+        signUpSignIn: "B2C_1_signup",
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_susi_reset_v2",
+            authority:  "https://fractalsauth.b2clogin.com/fractalsauth.onmicrosoft.com/B2C_1_signup",
         },
-        editProfile: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_edit_profile_v2"
-        }
     },
-    authorityDomain: "fabrikamb2c.b2clogin.com"
+    authorityDomain: "https://fractalsauth.b2clogin.com"
 }
