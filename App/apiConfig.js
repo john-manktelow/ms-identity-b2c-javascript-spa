527506b9a7835d1ce17a8de37534423850f6f612
John Manktelow <john+github@manktelow.co.nz>
2023-05-31 09:38:12 +1200

    scope to request and url for the API

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-    b2cScopes: ["https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read"],
-    webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
+    b2cScopes: ["https://fractalsauth.onmicrosoft.com/945cd4d6-db02-498f-a9d0-14e2c9ac5253/API.Access"],
+    webApi: "https://fractalsapipreview.azurewebsites.net/healthcheck/authping"
   };
